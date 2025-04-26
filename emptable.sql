cd1a0edc6961679436553946244200429a73521f
thotadatta1819 <dattathota1819@gmail.com>
2025-04-26 16:40:05 +0530

    Add files via upload

---
+++ emptable.sql
@@ -0,0 +1,6 @@
+select * from employee;
+create table employee(sno number(1),empname varchar2(20), empid varchar2(15), empsal number(15), emppno number(15));
+insert into employee values(4,'janaka','eid1821',22000,9182856928);
+
+
+
