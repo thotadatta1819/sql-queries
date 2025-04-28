a4589da5f9c3de973b45295b1d63e22cf51404b6
thotadatta1819 <dattathota1819@gmail.com>
2025-04-28 12:15:31 +0530

    Update emptable.sql

--- emptable.sql
+++ emptable.sql
@@ -2,5 +2,11 @@ select * from employee;
 create table employee(sno number(1),empname varchar2(20), empid varchar2(15), empsal number(15), emppno number(15));
 insert into employee values(4,'janaka','eid1821',22000,9182856928);
 
+select * from emp;
+create table emp(sno number(1),empname varchar2(20), empid varchar2(15), empsal number(15), emppno number(15));
+insert into emp values(4,'raghu','eid1922',21000,8531178654);
+update emp set emppno=9532269354 where sno=4;
+
+
 
 
